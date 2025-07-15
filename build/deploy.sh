3621b5937e9106b87050b935cd288db03b7be133
nirodu <NiroDu1219@outlook.com>
2025-07-15 11:58:28 +0800

    feat: add github action

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env sh
 
+# 确保脚本在错误时退出
+set -e
+
 # 生成静态文件
 yarn build
 
@@ -10,8 +13,14 @@ cd .vitepress/dist
 # echo 'nirodu.fun' > CNAME
 
 git init
+# 明确创建并切换到master分支
+git checkout -b master
 git config user.name 'nirodu'
 git config user.email 'nirodu1219@outlook.com'
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/NiroDu/n-book.git master:gh-pages
+
+# 使用SSH地址，从master分支推送到gh-pages分支
+git push -f git@github.com:NiroDu/n-book.git master:gh-pages
+
+cd -
